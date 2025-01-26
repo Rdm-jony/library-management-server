9aafe531a8158bcceeb577545603a450355a773b
Rdm-jony <jonydascse@gmail.com>
2025-01-26 23:05:54 +0600

    add production url origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','https://jovial-piroshki-143f3e.netlify.app'],
   credentials: true,
 
 }))
