051812f34c3d9d57d2de5e54a5a440b9e909d37c
Rdm-jony <jonydascse@gmail.com>
2025-01-27 00:10:18 +0600

    vercel

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173','https://library-management-85bef.web.app/'],
+  origin: ['http://localhost:5173','https://library-management-85bef.web.app'],
   credentials: true,
 
 }))
